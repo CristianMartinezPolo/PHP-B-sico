562ce214386d3090e16be9e0cbeff69288e3744a
CristianMartinezPolo <cristianmartinez9519@outlook.com>
2024-03-16 06:56:18 -0500

    Tipos de variables

---
+++ tipos.php
@@ -0,0 +1,19 @@
+<?php
+
+    /* $numero = "9";
+
+    $nuevo_numero = $numero + 2;
+
+    var_dump($numero);
+    var_dump($nuevo_numero); */
+
+    /* $numero = 10;
+    $numero = $numero +0.5;
+    var_dump($numero);
+    echo "\n" */
+
+    $papas = "10 papas en el costal";
+    $cuantas_papas_hay = $papas + 5;
+
+    echo $cuantas_papas_hay;
+?>
