8c2698cd0eaac1271ff42d348b4dc58f91ff5705
CristianMartinezPolo <cristianmartinez9519@outlook.com>
2024-03-16 06:17:01 -0500

    Renombramiento de index.php para almacenarlo como sintaxis-basica.php

---
+++ sintaxis-basica.php
@@ -0,0 +1,29 @@
+<?php
+    /* echo "Hola mundo"."<br>";
+    
+    $nombre = "Cristian";
+    $apellido = "Martínez"; */
+
+    //echo "Nombre: ".$nombre."<br>"."Apellido: ".$apellido."<br>";
+    // echo "Nombre: $nombre $apellido <br>"
+
+    //echo "El resultado de 4 * 5 es: " . (4*5) . "<br>";
+
+    $personas = [
+        "Carlos" => 22,
+        "Cristian" => 28,
+        "Juan" => 65
+    ];
+
+    //var_dump($personas);
+    /* print_r($personas);
+    echo "\n"; */
+
+    //Esto es un comentario
+    #Esto es otro comentario
+    /* ESto es 
+    un comentario 
+    de 
+    varias líneas */
+    
+?>
