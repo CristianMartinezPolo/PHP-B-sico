b2126436f50d15942d4716597635a8f9ea95fba8
CristianMartinezPolo <cristianmartinez9519@outlook.com>
2024-03-16 06:27:03 -0500

    Archivo variables.php

---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php
+    
+    $numero_1 = 8;
+    $numero_2 = 7;
+    $nombre = "Cristian";
+
+    define("NUMERO_PI", 3.1416);
+
+    echo $numero_1 + $numero_2."\n";
+
+    echo NUMERO_PI."\n";
+?>
