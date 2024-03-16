e0f7f7d84d3fa84dc6e322c751930ce1d256fac6
CristianMartinezPolo <cristianmartinez9519@outlook.com>
2024-03-16 07:30:05 -0500

    Casting de datos

---
+++ casting.php
@@ -0,0 +1,33 @@
+<?php
+    /* $numero = "45";
+
+
+    var_dump($numero);
+
+    $numero = (int)$numero;
+
+    echo "\n";
+
+    var_dump($numero);
+    echo "\n"; */
+
+    /* $dias = 5.89;
+    var_dump($dias);
+    echo "\n";
+
+    $dias = (int) $dias;
+    var_dump($dias);
+    echo "\n"; */
+
+    /* $bandera = 465411;
+    $bandera = (bool) $bandera;
+    var_dump($bandera);
+    echo "\n"; */
+
+    $numero_preguntas = 5 + "5";
+    var_dump($numero_preguntas);
+    echo "\n";
+    $numero_preguntas = "5" + 5;
+    var_dump($numero_preguntas);
+    echo "\n";
+?>
